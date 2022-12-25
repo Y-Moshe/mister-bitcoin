fb960611520a4fc99a17d4230920ad2e2c076bfe
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-25 19:21:22 +0200

    define complete routes

---
+++ src/pages/ChartPage.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ChartPage extends Component {
+  render() {
+    return (
+      <div>ChartPage</div>
+    )
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 
 import Root from '../Root'
-import HomePage from '../views/HomePage'
+import HomePage from '../pages/HomePage'
+import ContactPage from '../pages/ContactPage'
+import ContactDetailsPage from '../pages/ContactDetailsPage'
+import ContactEditPage from '../pages/ContactEditPage'
+import ChartPage from '../pages/ChartPage'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: '/',
     element: <Root />,
@@ -12,6 +16,22 @@ const router = createBrowserRouter([
       {
         path: 'home',
         element: <HomePage />
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />
+      },
+      {
+        path: 'contact/:id',
+        element: <ContactDetailsPage />
+      },
+      {
+        path: 'contact/edit/:id?',
+        element: <ContactEditPage />
+      },
+      {
+        path: 'chart',
+        element: <ChartPage />
       }
     ]
   }
