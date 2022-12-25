9514a1ed38070f3c6324b190b849ca61a4a99f49
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-25 18:51:46 +0200

    rename to Root

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
 import './assets/styles/main.scss'
+import AppHeader from './components/AppHeader'
 
 function Root() {
   return (
     <div className="main-layout">
-      <header>Header</header>
+      <AppHeader />
       <Outlet />
       <footer>Footer</footer>
     </div>
