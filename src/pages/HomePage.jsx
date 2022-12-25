bc03555adfa22be717b4d65912f9effe02c6757d
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-25 20:00:10 +0200

    basic homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
+import { Icon, Spinner } from '@blueprintjs/core'
+
+import { userService } from '../services/user.service'
+import { bitcoinService } from '../services/bitcoin.service'
 
 export default class HomePage extends Component {
+  state = {
+    user: null,
+    BTCRate: 0
+  }
+
+  componentDidMount() {
+    const user = userService.getUser()
+    this.setState({ user }, () => this.setBTCRate(user.coins))
+  }
+
+  setBTCRate = async (coins) => {
+    const rate = await bitcoinService.getRate(coins)
+    this.setState({ BTCRate: rate })
+    console.log(rate);
+  }
+
   render() {
+    const { user, BTCRate } = this.state
+    if (!user) return <Spinner intent='primary' />
     return (
-      <div>HomePage</div>
+      <div>
+        <h1>Hello {user.name}</h1>
+        <p><Icon icon='dollar' /> {user.coins}</p>
+        <p>BTC: {BTCRate}</p>
+      </div>
     )
   }
 }
