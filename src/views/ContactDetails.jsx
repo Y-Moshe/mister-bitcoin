77ef03f943fb37ca54e7d997634df656dbb4b463
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-26 15:58:16 +0200

    support contact actions

--- src/views/ContactDetails.jsx
+++ src/views/ContactDetails.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Spinner } from '@blueprintjs/core'
+import { Link } from 'react-router-dom'
+import { Spinner, Button, ButtonGroup } from '@blueprintjs/core'
 
 import { contactService } from '../services/contact.service'
 
@@ -18,15 +19,35 @@ export default class ContactDetails extends Component {
     this.setState({ contact })
   }
 
+  handleContactRemove = async () => {
+    await contactService.deleteContact(this.state.contact._id)
+    this.props.history.replace('/contact') // so going back is not an option!
+  }
+
   render() {
     const { contact } = this.state
     if (!contact) return <Spinner intent='primary' />
 
     return (
       <section>
-        <h4>Name: {contact.name}</h4>
-        <p>E-Mail: {contact.email}</p>
-        <p>Phone: {contact.phone}</p>
+        <div>
+          <h4>Name: {contact.name}</h4>
+          <p>E-Mail: {contact.email}</p>
+          <p>Phone: {contact.phone}</p>
+        </div>
+
+        <section className='flex justify-between'>
+          <Link to={'/contact'} replace>
+            <Button icon='arrow-left' intent='primary' />
+          </Link>
+
+          <ButtonGroup>
+            <Button icon='trash' intent='danger' onClick={this.handleContactRemove} />
+            <Link to={`/contact/edit/${contact._id}`}>
+              <Button icon='edit' intent='warning' />
+            </Link>
+          </ButtonGroup>
+        </section>
       </section>
     )
   }
