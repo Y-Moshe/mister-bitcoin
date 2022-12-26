61680521979de73a2bd094c03b5e2c664f8606cc
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-26 11:50:03 +0200

    basic charts

---
+++ src/components/Chart.jsx
@@ -0,0 +1,37 @@
+import {
+  AreaChart, XAxis, YAxis,
+  Tooltip, Area, Legend,
+  Line, LineChart
+} from 'recharts'
+
+export default function Chart({ title, description, data, color, type }) {
+  if (type === 'transactions') {
+    return (
+      <LineChart width={730} height={300} data={data}>
+        <XAxis dataKey="month" />
+        <YAxis dataKey="BIT" />
+        <Tooltip formatter={(value, name) => [`${value}`, name]} />
+        <Legend />
+        <Line type="monotone" dataKey="BIT" stroke="#8884d8" />
+      </LineChart>
+    )
+  }
+
+  return (
+    <AreaChart
+      width={730}
+      height={300}
+      data={data}>
+      <defs>
+        <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+          <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+        </linearGradient>
+      </defs>
+      <XAxis dataKey="month" angle={-45} tickMargin={15} height={50} />
+      <YAxis dataKey="USD" />
+      <Tooltip formatter={(value, name) => [`$${value}`, name]} />
+      <Area type="monotone" dataKey="USD" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+    </AreaChart>
+  )
+}
--- src/pages/StatisticPage.jsx
+++ src/pages/StatisticPage.jsx
@@ -1,9 +1,54 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+
+import { bitcoinService } from '../services/bitcoin.service'
+import Chart from '../components/Chart'
 
 export default class StatisticPage extends Component {
+  state = {
+    marketChartData: [],
+    transactionsChartData: []
+  }
+
+  componentDidMount() {
+    this.loadMarketChart()
+    this.loadTransactionsChart()
+  }
+
+  loadMarketChart = async () => {
+    const { values } = await bitcoinService.getMarketPrice()
+
+    const data = values.map(({ x, y }) => {
+      return {
+        month: moment(new Date(x * 1000)).format('MMM DD'),
+        USD: y
+      }
+    })
+
+    setTimeout(() => this.setState({ marketChartData: data }), 1000)
+  }
+
+  loadTransactionsChart = async () => {
+    const { values } = await bitcoinService.getConfirmedTransactions()
+
+    const data = values.map(({ x, y }) => {
+      return {
+        month: moment(new Date(x * 1000)).format('MMM DD'),
+        BIT: y
+      }
+    })
+
+    setTimeout(() => this.setState({ transactionsChartData: data }), 1000)
+  }
+
   render() {
+    const { marketChartData, transactionsChartData } = this.state
+
     return (
-      <div>StatisticPage</div>
+      <div>
+        <Chart type="marketPrice" data={marketChartData} />
+        <Chart type="transactions" data={transactionsChartData} />
+      </div>
     )
   }
 }
