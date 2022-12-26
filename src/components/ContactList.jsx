2e335dc90110daf3faa5be9d4b98cc886e09dc72
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-26 15:56:09 +0200

    support first item using children

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ContactPreview from './ContactPreview'
 
-export default function ContactList(props) {
-  const contactList = props.contacts.map(contact => (
+export default function ContactList({ contacts, children }) {
+  const contactList = contacts.map(contact => (
     <li key={contact._id}>
       <ContactPreview contact={contact} />
     </li>
   ))
   return (
     <ul className='clean-list'>
+      { children }
       { contactList }
     </ul>
   )
