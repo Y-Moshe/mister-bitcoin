48d8ea3347d994c2c760bffd9be223af0c940571
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-26 11:49:54 +0200

    getMarketPrice, getConfirmedTransactions

--- src/services/bitcoin.service.js
+++ src/services/bitcoin.service.js
@@ -5,11 +5,11 @@ function getRate(coins) {
 }
 
 function getMarketPrice() {
-  
+  return httpService.get(`https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true`)
 }
 
 function getConfirmedTransactions() {
-
+  return httpService.get(`https://api.blockchain.info/charts/transactions-per-second?timespan=5weeks&rollingAverage=8hours&format=json&cors=true`)
 }
 
 export const bitcoinService = {
