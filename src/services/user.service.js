93f79d176875929e41d44bba70e32f1e0686bd12
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-27 20:29:39 +0200

    add methods to userService

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,50 @@
-function getUser() {
-  return {
-    name: "Ochoa Hyde",
-    coins: 100,
-    moves: []
+import { utilService } from './util.service'
+
+const STORAGE_KEY = 'loggedInUser'
+const DEFAULT_USER_COINS = 100
+
+function signup(name) {
+  const user = {
+    _id: utilService.makeId(),
+    name,
+    coins: DEFAULT_USER_COINS,
+    moves: [],
+    settings: {
+      theme: 'light'
+    }
+  }
+
+  saveUser(user)
+  return user
+}
+
+function getLoggedInUser() {
+  return utilService.loadFromStorage(STORAGE_KEY)
+}
+
+function logout() {
+  return localStorage.removeItem(STORAGE_KEY)
+}
+
+function addMove(contact, amount) {
+  const user = getLoggedInUser()
+  if (user) {
+    user.moves.push({
+      contact,
+      amount,
+      at: Date.now()
+    })
+    saveUser(user)
   }
 }
 
+function saveUser(user) {
+  utilService.saveToStorage(STORAGE_KEY, user)
+}
+
 export const userService = {
-  getUser
+  signup,
+  getLoggedInUser,
+  logout,
+  addMove
 }
