3131e10a3662cc88ae248a2ae1bfe61273cf0f81
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-28 10:40:06 +0200

    wrapped with link

--- src/components/MoveList.jsx
+++ src/components/MoveList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import moment from 'moment'
 
 export default function MoveList({ title, moves, renderTo }) {
@@ -7,11 +8,13 @@ export default function MoveList({ title, moves, renderTo }) {
       { moves.length > 0 && <h3>{title}</h3>}
       {
         moves.map(({ contact, amount, at }) => (
-          <article className='move-preview' key={at}>
-            { renderTo && <p>To: {contact.name}</p> }
-            <p>{moment(new Date(at)).fromNow()}</p>
-            <p>Amount: {amount}</p>
-          </article>
+          <Link to={'/contact/' + contact._id}>
+            <article className='move-preview' key={at}>
+              { renderTo && <p>To: {contact.name}</p> }
+              <p>{moment(new Date(at)).fromNow()}</p>
+              <p>Amount: {amount}</p>
+            </article>
+          </Link>
         ))
       }
     </section>
