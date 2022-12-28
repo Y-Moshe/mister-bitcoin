c7e8b153e607ec3d64c3714a9718bacaa0182121
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-28 10:08:41 +0200

    responsive chart

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from 'react-responsive'
 import {
   ComposedChart, XAxis, YAxis,
   Tooltip, Area, Line, LineChart,
@@ -19,12 +20,21 @@ const CustomTooltip = ({ active, payload: data }) => {
 }
 
 export default function Chart({ title, description, data, color, type }) {
+  const isWide = useMediaQuery({ minWidth: 768 })
+  const size = {
+    width: isWide ? 730 : 300,
+    height: isWide ? 300 : 200
+  }
+
   let chartToRender = null
-  // #8884d8
+
   switch (type) {
     case 'transactions':
       chartToRender = (
-        <LineChart width={730} height={300} data={data}>
+        <LineChart
+          width={size.width}
+          height={size.height}
+          data={data}>
           <XAxis dataKey="month" />
           <YAxis dataKey="BIT" />
           <Tooltip formatter={(value, name) => [`${value.toFixed(7)}`, name]} animationEasing='ease-out' />
@@ -34,32 +44,13 @@ export default function Chart({ title, description, data, color, type }) {
       break;
     case 'marketPrice':
       chartToRender = (
-        // <AreaChart
-        //   width={730}
-        //   height={300}
-        //   data={data}>
-        //   <defs>
-        //     <linearGradient id="colorUsd" x1="0" y1="0" x2="0" y2="1">
-        //       <stop offset="5%" stopColor={color} stopOpacity={0.8} />
-        //       <stop offset="95%" stopColor={color} stopOpacity={0} />
-        //     </linearGradient>
-        //     <linearGradient id="colorUd" x1="0" y1="0" x2="0" y2="1">
-        //       <stop offset="5%" stopColor={'#8884d8'} stopOpacity={0.8} />
-        //       <stop offset="95%" stopColor={'#8884d8'} stopOpacity={0} />
-        //     </linearGradient>
-        //   </defs>
-        //   <XAxis dataKey="month" angle={-45} tickMargin={15} height={50} />
-        //   <YAxis />
-        //   <Tooltip formatter={(value, name) => [`$${value}`, name]} />
-        //   <Area type="monotone" dataKey="USD" stroke={color} fillOpacity={1} fill="url(#colorUsd)" />
-        //   <Area type="monotone" stroke={'#8884d8'} fillOpacity={1} fill="url(#colorUd)" />
-        // </AreaChart>
-        <ComposedChart width={730} height={300} data={data}>
+        <ComposedChart
+          width={size.width}
+          height={size.height}
+          data={data}>
           <XAxis dataKey="month" angle={-45} tickMargin={15} height={50} />
           <YAxis dataKey="USD" />
-          <Tooltip
-            content={CustomTooltip}
-            animationEasing='linear' />
+          <Tooltip content={CustomTooltip} animationEasing='linear' />
           <Area type="monotone" dataKey="USD" fill="#8884d8" stroke="#8884d8" />
           <Bar dataKey="USD" barSize={20} fill="#413ea0" />
           <Line type="monotone" dataKey="USD" stroke="#ff7300" />
