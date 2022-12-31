08de026862078a0d0cf2d7fbd5899def2a624cdb
Y-Moshe <moshe.nehemiah@gmail.com>
2023-01-01 00:00:07 +0200

    fix transition error

--- src/components/MoveList.jsx
+++ src/components/MoveList.jsx
@@ -5,21 +5,23 @@ import moment from 'moment'
 
 export default function MoveList({ title, moves, renderTo }) {
   return (
-    <TransitionGroup className={'move-list'} component='section'>
+    <>
       {moves.length > 0 && <h3>{title}</h3>}
-      {
-        moves.map(({ _id, contact, amount, at }) => (
-          <CSSTransition classNames='slide-in' key={_id} timeout={500}>
-            <Link to={'/contact/' + contact._id}>
-              <article className='move-preview'>
-                {renderTo && <p>To: {contact.name}</p>}
-                <p>{moment(new Date(at)).fromNow()}</p>
-                <p>Amount: {amount}</p>
-              </article>
-            </Link>
-          </CSSTransition>
-        ))
-      }
-    </TransitionGroup>
+      <TransitionGroup className={'move-list'} component='section'>
+        {
+          moves.map(({ _id, contact, amount, at }) => (
+            <CSSTransition classNames='slide-in' key={_id} timeout={500}>
+              <Link to={'/contact/' + contact._id}>
+                <article className='move-preview'>
+                  {renderTo && <p>To: {contact.name}</p>}
+                  <p>{moment(new Date(at)).fromNow()}</p>
+                  <p>Amount: {amount}</p>
+                </article>
+              </Link>
+            </CSSTransition>
+          ))
+        }
+      </TransitionGroup>
+    </>
   )
 }
